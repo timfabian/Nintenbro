4e24ff9e9e18b3c843a0ef4de6c5139c90f6d2ae
timfabian <timfabian@ucla.edu>
2014-10-24 00:49:33 -0700

    some connection stuff to go with test server

--- src/com/wdc/nintenbro/MainActivity.java
+++ src/com/wdc/nintenbro/MainActivity.java
@@ -1,14 +1,19 @@
 package com.wdc.nintenbro;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.net.InetAddress;
+import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Random;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -19,9 +24,23 @@ import android.widget.Toast;
 public class MainActivity extends ActionBarActivity {
 	private Item mCurrentItem;
 	private Socket mSocket;
+	private ServerSocket mServerSocket;
+	Handler updateConversationHandler;
 	
-	private static final int SERVERPORT = 5000;
-    private static final String SERVER_IP = "10.0.2.2";
+	// adb will have the first emulator launcher on port 5554 of the localhost
+	// second emulator will be port 5556
+	
+	// telnet into the server emulator
+	// redir add tcp:5000:6000
+	
+	// Port to open on the emulator's IP
+	public static final int SERVERPORT = 6000;
+	
+	// 10.0.2.2:5000 is the alias for the localhost
+	private static final int TARGET_PORT = 5000;
+    private static final String TARGET_IP = "10.0.2.2";
+    
+    private static final boolean mServerModeFlag = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,10 +50,39 @@ public class MainActivity extends ActionBarActivity {
         // Initialize
         mCurrentItem = Item.NULL;
         
-        // Start the client thread to open a socket
-        new Thread( new ClientThread() ).start();
+        if ( mServerModeFlag == false ) {
+        
+	        // Start the client thread to open a socket
+	        new Thread( new ClientThread() ).start();
+        
+        }
+        else {
+        	
+        	updateConversationHandler = new Handler();
+        
+	        // Start listening on the server thread
+	        new Thread( new ServerThread() ).start();
+        
+        }
 
     }
+    
+    @Override
+	protected void onStop() {
+		super.onStop();
+		
+		try {
+			
+			if ( mServerModeFlag == false )
+				mSocket.close();
+			else
+				mServerSocket.close();
+		} 
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
 
     class ClientThread implements Runnable {
     	
@@ -44,10 +92,10 @@ public class MainActivity extends ActionBarActivity {
     		try {
     			
     			// TODO - IP address
-    			InetAddress servAddr = InetAddress.getByName(SERVER_IP);
+    			InetAddress servAddr = InetAddress.getByName(TARGET_IP);
     			
     			// TODO - port number
-    			mSocket = new Socket(servAddr, SERVERPORT);
+    			mSocket = new Socket(servAddr, TARGET_PORT);
     			
     		}
     		// TODO - better error handling
@@ -58,6 +106,88 @@ public class MainActivity extends ActionBarActivity {
     	}
     	
     }
+    
+    class ServerThread implements Runnable {
+
+		public void run() {
+			Socket socket = null;
+			
+			try {
+				mServerSocket = new ServerSocket(SERVERPORT);
+			} 
+			catch (Exception e) {
+				e.printStackTrace();
+			}
+			
+			while ( !Thread.currentThread().isInterrupted() ) {
+
+				try {
+					socket = mServerSocket.accept();
+					
+					Log.v("Nintenbro", "server socket accepted connection");
+
+					InputCommunicationThread commThread = new InputCommunicationThread( socket );
+					new Thread( commThread ).start();
+				} 
+				catch ( Exception e ) {
+					e.printStackTrace();
+				}
+				
+			}
+			
+		}
+		
+	} // end class ServerThread
+    
+    class InputCommunicationThread implements Runnable {
+		private Socket clientSocket;
+		private BufferedReader input;
+
+		public InputCommunicationThread( Socket clientSocket ) {
+
+			this.clientSocket = clientSocket;
+
+			try {
+				this.input = new BufferedReader( new InputStreamReader( this.clientSocket.getInputStream() ) );
+			} 
+			catch ( Exception e ) {
+				e.printStackTrace();
+			}
+			
+		}
+
+		public void run() {
+
+			while ( !Thread.currentThread().isInterrupted() ) {
+
+				try {
+					String read = input.readLine();
+					Log.v("Nintenbro", "read " + read);
+					updateConversationHandler.post( new updateUIThread( read ) );
+				} 
+				catch ( Exception e ) {
+					e.printStackTrace();
+				}
+				
+			}
+			
+		}
+
+	} // end class Communication Thread
+    
+    class updateUIThread implements Runnable {
+		private String msg;
+
+		public updateUIThread( String str ) {
+			this.msg = str;
+		}
+
+		@Override
+		public void run() {
+			Log.v("Nintenbro", "Client Says: "+ msg);
+		}
+		
+	} // end class updateUIThread
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -80,6 +210,8 @@ public class MainActivity extends ActionBarActivity {
     
     public void sendMessage(View view) {
     	
+    	Log.v("Nintenbro", "sendMessage");
+    	
     	if ( mCurrentItem == Item.NULL ) {
     		
     		// No item would mean no message
@@ -91,6 +223,7 @@ public class MainActivity extends ActionBarActivity {
     		
     	}
     	else {
+    		
     		try {
     			
     			// TODO - message protocol?
